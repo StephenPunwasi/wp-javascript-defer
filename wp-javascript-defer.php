c709463fc8c8b6f46243a7139d4a4ef906fc8e16
Stephen Punwasi <stephen.punwasi@gmail.com>
2014-11-22 20:00:37 -0500

    Init Commit

---
+++ wp-javascript-defer.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: WP Async
+Plugin URI: http://www.stephenpunwasi.com/
+Version: 0.0.1
+Author: Stephen Punwasi
+Description: Load JavaScript Files Asynchronously.
+*/
+javascript on page load
+?>
